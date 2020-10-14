bd2fd736b13b7387c0ede0a021572cb55183a40a
Daniel Assis <daniel.assis@inatel.br>
2020-10-13 23:42:26 -0300

    Separate checked and unchecked items

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -16,38 +16,42 @@ export default class HtmlService {
     }
 
     async listItems() {
-        const items =  await this.groceryListService.getAll();
+        const items = await this.groceryListService.getAll();
         items.forEach(item => this.addToHtmlList(item));
     }
 
-    async addItem(product){
-        const item = {product, checked: false};
+    async addItem(product) {
+        const item = { product, checked: false };
         const itemId = await this.groceryListService.save(item);
         item.id = itemId;
         this.addToHtmlList(item);
+        
     }
 
-    async saveItem(itemId, isChecked){
+    async saveItem(itemId, isChecked) {
         const item = await this.groceryListService.get(itemId);
         item.checked = isChecked;
         this.groceryListService.save(item);
+        this.addToHtmlList(item);
     }
 
-    async deleteTask(li){
+    async deleteTask(li) {
         const itemId = +li.getAttribute('data-item-id');
         await this.groceryListService.delete(itemId);
         li.remove();
     }
 
-    toggleItem(li){
+    toggleItem(li) {
         const itemId = +li.getAttribute('data-item-id');
         li.classList.toggle('checked');
         const isChecked = li.classList.contains('checked');
         this.saveItem(itemId, isChecked);
+        li.remove();
     }
 
-    addToHtmlList(item){
-        const ul = document.querySelector('ul');
+    addToHtmlList(item) {
+        const ulUnchecked = document.getElementById('unchecked-items');
+        const ulChecked = document.getElementById('checked-items');
         const li = document.createElement('li');
         const span = document.createElement('span');
         const button = document.createElement('button');
@@ -62,12 +66,16 @@ export default class HtmlService {
             this.deleteTask(li);
         })
 
-        if (item.checked){
+        if (item.checked) {
             li.classList.add('checked')
+            li.appendChild(span);
+            li.appendChild(button);
+            ulUnchecked.appendChild(li);
         }
-
-        li.appendChild(span);
-        li.appendChild(button);
-        ul.appendChild(li);
+        else {
+            li.appendChild(span);
+            li.appendChild(button);
+            ulChecked.appendChild(li);
+        }
     }
 }
