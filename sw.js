5294532b4e6d6dec2a21e2fb4c4dda3f223bbcd1
Daniel Assis <daniel.assis@inatel.br>
2020-10-17 09:43:20 -0300

    Add missing element to cache

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const cacheName = 'app-shell-v2';
 const assetsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
   'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
